2375d25f56b0951d95239ee601968c9e14a7b875
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-04-18 21:27:19 +0530

    quiz section

---
+++ src/@quiz/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import React from "react";
+
+export default function quiz() {
+  return (
+    <section className="flex flex-col justify-center items-center mt-10">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Question No{" "}
+        <span className="text-blue-600 dark:text-blue-500"> #1</span>.
+      </h1>
+      <p>Score : 0</p>
+      <section className="shadow-2xl my-10 p-10 w-[90%] rounded-lg flex flex-col justify-center items-center shadow-blue-200">
+        <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-blue-600 dark:text-blue-500 md:text-3xl lg:text-3xl">
+          Thios is the first question
+        </h1>
+        <div className="flex justify-center items-center my-10 flex-wrap w-[90%]]">
+          <button
+            type="button"
+            className=" w-[33%] border-0 my-4 py-3.5 px-5 me-2 mb-2 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-full border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Alternative
+          </button>
+          <button
+            type="button"
+            className=" w-[33%] border-0 my-4 py-3.5 px-5 me-2 mb-2 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-full border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Alternative
+          </button>
+          <button
+            type="button"
+            className=" w-[33%] border-0 my-4 py-3.5 px-5 me-2 mb-2 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-full border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Alternative
+          </button>
+          <button
+            type="button"
+            className=" w-[33%] border-0 my-4 py-3.5 px-5 me-2 mb-2 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-full border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Alternative
+          </button>
+        </div>
+        <button
+            type="button"
+            className=" w-[33%] py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Next
+          </button>
+      </section>
+    </section>
+  );
+}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,14 @@
+"use client"
+
+import useQuiz from "@/app/store";
 import React from "react";
 
 export default function Button() {
+  const addStatus = useQuiz(state => state.addStatus);
   return (
     <button
       type="button"
+      onClick={() => addStatus("start")}
       className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
     >
       Start Quiz
